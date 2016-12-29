c8a75026de70de6689886e6bf8f01630cb3077a4
Your Name <you@example.com>
2016-12-30 00:49:26 +0900

    ログイン機能。イベント新規作成機能。バリデーション機能。エラーMSG。日本語化。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,27 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :logged_in?, :current_user
+
   private
+
+  def current_user
+    return unless session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+  end
+
+  # def current_user
+  #   if session[:user_id]
+  #     @current_user ||= User.find(session[:user_id])
+  #   end
+  # end
+
   def logged_in?
     !!session[:user_id]
   end
-  helper_method :logged_in?
 
-  # def current_member
-  #   User.find_by(id: session[:user_id]) if session[:user_id]
-  # end
-  #   helper_method :current_member
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: "ログインしてください"
+  end
 end
