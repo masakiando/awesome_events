61932b069cf0d61408bd96a911732c5ed12853a7
Your Name <you@example.com>
2016-12-31 02:03:50 +0900

    イベントの編集、更新、削除機能実装。

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,24 @@
 class EventsController < ApplicationController
   before_action :authenticate, except: [:show]
 
-  # event詳細
+  # 一覧 index
+
+  # 詳細 show
   def show
     @event = Event.find(params[:id])
   end
 
-  # 新規登録フォーム
+  # 新規フォーム new
   def new
     @event = current_user.created_events.build
   end
 
+  # 編集フォーム edit
+  def edit
+    @event = current_user.created_events.find(params[:id])
+  end
+
+  # 新規レコード作成 create
   def create
     @event = current_user.created_events.build(event_params)
     if @event.save
@@ -18,6 +26,23 @@ class EventsController < ApplicationController
     else
       render :new
     end
+  end
+
+  # 更新 update
+  def update
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: "更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  # 削除 destroy
+  def destroy
+    @event = current_user.created_events.find(params[:id])
+    @event.destroy
+    redirect_to :root, notice: "削除しました。"
   end
 
   private
